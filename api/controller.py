89fdd399a07c1209a6cbb6e22482f07f4ef99bc5
vondarten <paulo.henrique302@outlook.com>
2025-09-26 18:19:23 -0300

    Added CUDA support

--- api/controller.py
+++ api/controller.py
@@ -14,23 +14,24 @@ logger = logging.getLogger(__name__)
 
 class ColorizationController:
     def __init__(self):
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         self.model = self._load_model()
 
     def _load_model(self) -> DynamicUnet:
-        logger.info('Loading model...')
+        logger.info(f'Loading model to {self.device}')
         tic = time.time()
         basemodel = models.resnet18(weights=None)
         body = create_body(basemodel, n_in=1, pretrained=False, cut=-2) 
-        model = DynamicUnet(body, 2, (384, 384), self_attention=False, act_cls=torch.nn.Mish).to('cpu')
+        model = DynamicUnet(body, 2, (384, 384), self_attention=False, act_cls=torch.nn.Mish).to(self.device)
         experiment = 'gan-unet-resnet18-just-mish-4-2024-07-18-17:45:12'
         
         model_path = f"./experiments/{experiment}/model.pth"
         if not os.path.exists(model_path):
             raise FileNotFoundError(f"Model file not found at {model_path}")
             
-        saved_model = torch.load(model_path, map_location='cpu')
+        saved_model = torch.load(model_path, map_location=self.device)
         model.load_state_dict(saved_model['model_state_dict'])
-        logger.info(f">>> Model loaded successfully in {time.time() - tic:.4f}s")
+        logger.info(f">>> [{self.device}] Model loaded successfully in {time.time() - tic:.4f}s")
         return model
 
     def colorize_image(self, img: Image) -> Image:
@@ -50,7 +51,7 @@ class ColorizationController:
 
         logger.info('Converted original and resized images to LAB color space')
         
-        sample = {'L': resized_lab['L'].unsqueeze(0).to('cpu')}
+        sample = {'L': resized_lab['L'].unsqueeze(0).to(self.device)}
 
         model_tic = time.time()
 
@@ -61,7 +62,7 @@ class ColorizationController:
         logger.info(f'>>> Model inference done in {time.time() - model_tic:4f}s')
 
         # Upsample the generated ab channels to the original image size
-        model_ab = torch.nn.functional.interpolate(model_ab, size=(height, width), mode='bilinear')
+        model_ab = torch.nn.functional.interpolate(model_ab, size=(height, width), mode='bilinear').cpu()
 
         # Combine the original L channel with the upsampled ab channels
         colorized_img_np = lab_to_rgb(orig_L.unsqueeze(0), model_ab).squeeze(0)
